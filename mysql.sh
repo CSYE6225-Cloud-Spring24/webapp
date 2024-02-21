5045145adaa42d53735658ed39fb0fd3f05fa400
Keerthana Mikkili <mikkili.k@northeastern.edu>
2024-02-21 07:55:26 -0500

    Added mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sudo dnf update -y
+
 sudo dnf upgrade -y
 sudo dnf install java-17-openjdk -y
 
@@ -15,30 +15,7 @@ mysql -u root  -e "FLUSH PRIVILEGES;"
 # Start and enable MySQL service
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
- 
-# # Configure the SQL for the first time
-# SECURE_INSTALLATION=$(expect -c "
-# spawn sudo mysql_secure_installation
-# expect \"Enter current password for root (enter for none):\"
-# send \"\r\"
-# expect \"Set root password?\"
-# send \"y\r\"
-# expect \"New password:\"
-# send \"web-app\r\"
-# expect \"Re-enter new password:\"
-# send \"web-app\r\"
-# expect \"Remove anonymous users?\"
-# send \"y\r\"
-# expect \"Disallow root login remotely?\"
-# send \"y\r\"
-# expect \"Remove test database and access to it?\"
-# send \"y\r\"
-# expect \"Reload privilege tables now?\"
-# send \"y\r\"
-# expect eof
-# ")
- 
-echo "$SECURE_INSTALLATION"
+
 
 
 
