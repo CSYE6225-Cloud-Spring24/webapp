ad53f4344b884980c55994be0e2f9bb52352624b
Keerthana Mikkili <mikkili.k@northeastern.edu>
2024-03-19 22:30:19 -0400

    Added severity

--- OpsAgent.sh
+++ OpsAgent.sh
@@ -2,8 +2,8 @@
 
 sudo chown -R csye6225:csye6225 /etc/google-cloud-ops-agent/
 
-sudo mkdir -p /var/logs/webapp/
-sudo chown -R csye6225:csye6225 /var/logs/webapp/
+sudo mkdir -p /var/log/webapp/
+sudo chown -R csye6225:csye6225 /var/log/webapp/
 
 cat <<EOF | sudo tee /etc/google-cloud-ops-agent/config.yaml
 logging:
@@ -11,18 +11,23 @@ logging:
     my-app-receiver:
       type: files
       include_paths:
-        - /var/logs/webapp/application.log
+        - /var/log/webapp/application.log
       record_log_file_path: true
   processors:
     my-app-processor:
       type: parse_json
       time_key: time
       time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+    move_severity:
+      type: modify_fields
+      fields:
+        severity:
+          move_from: jsonPayload.severity
   service:
     pipelines:
       default_pipeline:
         receivers: [my-app-receiver]
-        processors: [my-app-processor]
+        processors: [my-app-processor,move_severity]
 EOF
 
 sudo systemctl restart google-cloud-ops-agent
