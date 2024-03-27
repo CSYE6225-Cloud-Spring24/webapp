900b2b0dd9a2d28e1a5c25d1c4a27498aae4b605
Keerthana Mikkili <mikkili.k@northeastern.edu>
2024-03-27 05:08:12 -0400

    Updated user service

--- src/main/java/com/webapplication/Webapp/service/UserService.java
+++ src/main/java/com/webapplication/Webapp/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 public interface UserService {
@@ -15,5 +17,9 @@ public interface UserService {
     public User createUser(User user)throws Exception;
 
     public boolean ValidCredentials(String username, String password);
+
+    Optional<User> getUserById(UUID id);
+
+    void saveUser(User user);
 
 }
